5438c2ba4da2ad740ab7dabccda0481d80a86f57
Luke Rens <31190219+RensLuke@users.noreply.github.com>
2019-05-17 13:27:04 -0500

    final version

--- reoptimize.py
+++ reoptimize.py
@@ -14,7 +14,7 @@ optimizedData = []
 
 # TODO add header block with a description of what insertconstraints does
 
-def insertconstraints(ATOM_INDEX):
+def insertconstraints(ATOM_INDEX, doesConstraints):
 
     # TODO comment code
 
@@ -53,37 +53,65 @@ def insertconstraints(ATOM_INDEX):
                 counter = counter + 1
 
     # conditional statement so file is only manipulated if user enters a constraint
-    if int(number_of_constraints) > 0:
+    if doesConstraints and int(number_of_constraints) > 0:
+        for i in range(len(newoptimizeddata)):
+            if i == 0:
+                optimizedData.insert(ATOM_INDEX - 1, newoptimizeddata[len(newoptimizeddata) - 1 - i] + '\n')
+            else:
+                optimizedData.insert(ATOM_INDEX - 1, newoptimizeddata[len(newoptimizeddata) - 1 - i] + '\n')
+        optimizedData.insert(ATOM_INDEX+int(number_of_constraints)-1, "END\n")
+    elif int(number_of_constraints) > 0:
         newoptimizeddata.insert(0, "CONSTRAINTS")
-        newoptimizeddata.append("END")
-
         for i in range(len(newoptimizeddata)):
             if i == 0:
                 optimizedData.insert(ATOM_INDEX - 2, newoptimizeddata[len(newoptimizeddata) - 1 - i])
             else:
                 optimizedData.insert(ATOM_INDEX - 2, newoptimizeddata[len(newoptimizeddata) - 1 - i] + '\n')
+        optimizedData.insert(ATOM_INDEX+1, "\n END")
 
 
-# optimizedData = input("Please select file you would like to optimize ")
+optimizedChild = " "
+doesConstraints = False
 
-# TODO directory selection, same as ADFtoReaxFF
+while True:
+    optimizedChild = input("Please enter input file path: ")
+    # optimizedChild = "ohmstedelv/water.run"
 
-optimizedChild = "C:/Users/Luke/PycharmProjects/ChemistryScripts/ohmstedelv\water.run"
+    exists = os.path.exists(optimizedChild)
+    if exists:
+        print("Correct path")
+        break
+    else:
+        print("Invalid file path")
 
-if os.path.exists(optimizedChild):
-    with open(optimizedChild, "r") as openFile:
-        for line in openFile:
-            optimizedData.append(line)
+
+with open(optimizedChild, "r") as openFile:
+    for line in openFile:
+        optimizedData.append(line)
 
 # TODO function to exchange for new XYZ coordinates in Atoms block
 
 for x in range(len(optimizedData)):
+    if "CONSTRAINTS" in optimizedData[x]:
+        doesConstraints = True
+    if doesConstraints and "END" in optimizedData[x]:
+        optimizedData.pop(x)
     if "Atoms" in optimizedData[x]:
-        insertconstraints(x)
+        insertconstraints(x, doesConstraints)
         break
 
-for x in range(len(optimizedData)):
-    print(optimizedData[x], end=" ")
+path_split = optimizedChild.split('/')
+print("Old file name was: " + path_split[len(path_split) - 1])
+newName = input("Enter New File Name (without .run): ")
+newName = newName + ".run"
+path_split[len(path_split) - 1] = newName
+newPath = '/'.join(path_split)
+
+with open(newPath, 'a+') as writer:
+    for x in range(len(optimizedData)):
+        writer.write(optimizedData[x])
+
+print("Done")
 
 
 
